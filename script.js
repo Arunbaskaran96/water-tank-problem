5a6d38c4e555930c3d5c846c611f8bb448ee5b5e
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2024-02-16 22:42:56 +0530

    added readme file

--- script.js
+++ script.js
@@ -1,178 +1,178 @@
-
-
-function fetchData(){
-    let temp=document.getElementById("input")
-    let input=temp.value.split(",")
-    brickswithwater(input)
-    Onlywater(input)
+function fetchArr() {
+  let fromElement = document.getElementById("inputArr");
+  let inputArr = fromElement.value.split(",");
+  let bricks = showBricksAndWater(inputArr);
+  let water = showOnlyWater(inputArr);
+  showBricksAndWater(inputArr, bricks);
+  showOnlyWater(inputArr, water);
 }
 
+const waterSum = (finalCase) => {
+  let sum = 0;
+  for (let i = 0; i < finalCase.length; i++) {
+    let element = finalCase[i];
+    if (element != "-") {
+      sum += +element;
+    }
+  }
+  return sum;
+};
 
-function createGraph(xaxis,yaxis,id){
-    var dom = document.getElementById(id);
-    var myChart = echarts.init(dom, null, {
-    renderer: 'canvas',
-    useDirtyRect: false
-    });
-    var option;
-    option = {
+function createChartTable(xAxisNamesArr, outputArr, id) {
+  let dom = document.getElementById(id);
+  let myChart = echarts.init(dom, "dark", {
+    renderer: "canvas",
+    useDirtyRect: false,
+  });
+  let option = {
     xAxis: {
-        type: 'category',
-        data: xaxis
+      type: "category",
+      data: xAxisNamesArr,
     },
     yAxis: {
-        type: 'value'
+      type: "value",
     },
     series: [
-        {
-        data: yaxis,
-        type: 'bar'
-        }
-    ]
-    };
-    if (option && typeof option === 'object') {
+      {
+        data: outputArr,
+        type: "bar",
+      },
+    ],
+  };
+  if (option && typeof option === "object") {
     myChart.setOption(option);
-    }
-    window.addEventListener('resize', myChart.resize);
+  }
+  window.addEventListener("resize", myChart.resize);
 }
 
-function waterQuantity(arr){
-    let sum=0
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]!="-"){
-            sum+=parseInt(arr[i])
-        }
+function showBricksAndWater(bricks) {
+  let finalCase = [];
+  let firstcase = [];
+  let secondCase = [];
+  let result = [];
+  let lastVlueForFirstCase = 0;
+  let lastVlueForSecondCase = 0;
+  for (let i = 0; i < bricks.length; i++) {
+    let brick = bricks[i];
+    if (brick == 0) {
+      firstcase.push(lastVlueForFirstCase);
+    } else {
+      firstcase.push("-");
+      lastVlueForFirstCase = brick;
     }
-    return sum
+  }
+  for (let i = bricks.length - 1; i >= 0; i--) {
+    let brick = bricks[i];
+    if (brick == 0) {
+      secondCase[i] = lastVlueForSecondCase;
+    } else {
+      secondCase[i] = "-";
+      lastVlueForSecondCase = brick;
+    }
+  }
+  for (let i = 0; i < bricks.length; i++) {
+    let fc = firstcase[i];
+    let sc = secondCase[i];
+    if (fc == "-") {
+      finalCase[i] = "-";
+    } else {
+      finalCase[i] = fc - sc > 0 ? sc : fc;
+    }
+  }
+  for (let i = 0; i < bricks.length; i++) {
+    let brick = bricks[i];
+    if (brick == 0) {
+      // water
+      result.push({
+        value: finalCase[i],
+        itemStyle: {
+          color: "#0000FF",
+        },
+      });
+    } else {
+      // brick
+      result.push({
+        value: brick,
+        itemStyle: {
+          color: "#FFFF00",
+        },
+      });
+    }
+  }
+  console.log(firstcase);
+  console.log(secondCase);
+  console.log(finalCase);
+  console.log(result);
+  createChartTable(bricks, result, "chart-container");
 }
 
-function brickswithwater(arr){
-    let firstCase=[]
-    let secondCase=[]
-    let finalCase=[]
-    let result=[]
-    let lastvalueoffirstcase=0
-    let lastvalueosecondcase=0
+// input  [0,6, 6, 0, 0, 7]
+// output [0, 0, 0, 6, 6, 0]
+// createChartTable(input, output, 'chart-container1')
 
-    for(let i=0;i<arr.length;i++){
-        let value=arr[i]
-        if(value==0){
-            firstCase.push(lastvalueoffirstcase)
-        }else{
-            firstCase.push("-")
-            lastvalueoffirstcase=value
-        }
+function showOnlyWater(water) {
+  let finalCase = [];
+  let firstcase = [];
+  let secondCase = [];
+  let result = [];
+  let lastVlueForFirstCase = 0;
+  let lastVlueForSecondCase = 0;
+  for (let i = 0; i < water.length; i++) {
+    let waterwall = water[i];
+    if (waterwall == 0) {
+      firstcase.push(lastVlueForFirstCase);
+    } else {
+      firstcase.push("-");
+      lastVlueForFirstCase = waterwall;
     }
-
-    for(let i=arr.length-1;i>=0;i--){
-        let value=arr[i]
-        if(value==0){
-            secondCase[i]=lastvalueosecondcase
-        }else{
-            secondCase[i]="-"
-            lastvalueosecondcase=value
-        }
+  }
+  for (let i = water.length - 1; i >= 0; i--) {
+    let waterwall = water[i];
+    if (waterwall == 0) {
+      secondCase[i] = lastVlueForSecondCase;
+    } else {
+      secondCase[i] = "-";
+      lastVlueForSecondCase = waterwall;
     }
-
-    for(let i=0;i<arr.length;i++){
-        let fisrt=firstCase[i]
-        let second=secondCase[i]
-        let value=arr[i]
-        if(value=="-"){
-            finalCase.push(value)
-        }else{
-            finalCase.push(fisrt-second >0 ? second : fisrt)
-        }
+  }
+  for (let i = 0; i < water.length; i++) {
+    let fc = firstcase[i];
+    let sc = secondCase[i];
+    if (fc == "-") {
+      finalCase[i] = "-";
+    } else {
+      finalCase[i] = fc - sc > 0 ? sc : fc;
     }
-
-
-    for(let i=0;i<arr.length;i++){
-        let val=arr[i]
-        if(val==0){
-            result.push({
-                value:finalCase[i],
-                itemStyle:{
-                    color:'blue'
-                }
-            })
-        }else{
-            result.push({
-                value:val,
-                itemStyle:{
-                    color:'gray'
-                }
-            })
-        }
+  }
+  for (let i = 0; i < water.length; i++) {
+    let waterwall = water[i];
+    if (waterwall == 0) {
+      // water
+      result.push({
+        value: finalCase[i],
+        itemStyle: {
+          color: "#0000FF",
+        },
+      });
+    } else {
+      // brick
+      result.push({
+        value: 0,
+        itemStyle: {
+          color: "#FFFF00",
+        },
+      });
     }
+  }
 
+  console.log(waterSum(finalCase));
+  console.log(firstcase);
+  console.log(secondCase);
+  console.log(finalCase);
+  console.log(result);
 
-    createGraph(arr,result,"chart1")
-    let temp=document.getElementById("waterquantity")
-    let txt=temp.innerHTML=`Total Water Quantity is ${waterQuantity(finalCase)}`
-    document.body.append(txt)
-}
+  createChartTable(water, result, "chart-container1");
 
-
-function Onlywater(arr){
-    let firstCase=[]
-    let secondCase=[]
-    let finalCase=[]
-    let result=[]
-    let lastvalueoffirstcase=0
-    let lastvalueosecondcase=0
-
-    for(let i=0;i<arr.length;i++){
-        let value=arr[i]
-        if(value==0){
-            firstCase.push(lastvalueoffirstcase)
-        }else{
-            firstCase.push("-")
-            lastvalueoffirstcase=value
-        }
-    }
-
-    for(let i=arr.length-1;i>=0;i--){
-        let value=arr[i]
-        if(value==0){
-            secondCase[i]=lastvalueosecondcase
-        }else{
-            secondCase[i]="-"
-            lastvalueosecondcase=value
-        }
-    }
-
-    for(let i=0;i<arr.length;i++){
-        let fisrt=firstCase[i]
-        let second=secondCase[i]
-        let value=arr[i]
-        if(value=="-"){
-            finalCase.push(value)
-        }else{
-            finalCase.push(fisrt-second >0 ? second : fisrt)
-        }
-    }
-
-
-    for(let i=0;i<arr.length;i++){
-        let val=arr[i]
-        if(val==0){
-            result.push({
-                value:finalCase[i],
-                itemStyle:{
-                    color:'blue'
-                }
-            })
-        }else{
-            result.push({
-                value:val-finalCase[i],
-                itemStyle:{
-                    color:'gray'
-                }
-            })
-        }
-    }
-
-
-    createGraph(arr,result,"chart2")
-
+  let outputspan = document.getElementById("waterunit");
+  outputspan.innerHTML = `Total water unit ${waterSum(finalCase)}`;
 }
